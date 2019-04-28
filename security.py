3d5943e9bd78cc9337b7151c0d3f59977d585448
use opencv <dsnurfaizi@gmail.com>
2019-04-28 11:09:58 +0700

    update security

---
+++ security.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np 
+import sqlite3
+import os
+import time
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+fname = "recognizer/trainingData.yml"
+if not os.path.isfile(fname):
+  print("Please train the data first")
+  exit(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read(fname)
+email_update_interval = 600
+last_epoch = 0 
+while True:
+  ret, img = cap.read()
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+  for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+    ids,conf = recognizer.predict(gray[y:y+h,x:x+w])
+    c.execute("select name from users where id = (?);", (ids,))
+    result = c.fetchall()
+    name = result[0][0]
+    if conf < 50:
+      cv2.putText(img, name, (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,255,0),2)
+      if conf <50 and name != "dais" and (time.time() - last_epoch) > email_update_interval:
+          last_epoch = time.time()
+          cv2.imwrite('opencv'+str(name)+'.jpg',img)
+          fromaddr = "sgoku231@gmail.com"
+          #toaddr = "bagaswahyuvidiasmoro@gmail.com"
+          toaddr = "lamunesseliot@gmail.com"
+          #bagaswahyuvidiasmoro@yahoo.com
+          # instance of MIMEMultipart 
+          msg = MIMEMultipart() 
+          # storing the senders email address   
+          msg['From'] = fromaddr 
+          # storing the receivers email address  
+          msg['To'] = toaddr 
+          # storing the subject  
+          msg['Subject'] = ''+str(name)+' mengunjungi rumah anda'
+          # string to store the body of the mail 
+          body = "security camera"
+          # attach the body with the msg instance 
+          msg.attach(MIMEText(body, 'plain')) 
+          # open the file to be sent  
+          filename = "test.jpg"
+          attachment = open('opencv'+str(name)+'.jpg', "rb") 
+          # instance of MIMEBase and named as p 
+          p = MIMEBase('application', 'octet-stream') 
+          # To change the payload into encoded form 
+          p.set_payload((attachment).read()) 
+          # encode into base64 
+          encoders.encode_base64(p) 
+          p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+          # attach the instance 'p' to instance 'msg' 
+          msg.attach(p) 
+          # creates SMTP session 
+          s = smtplib.SMTP('smtp.gmail.com', 587) 
+          # start TLS for security 
+          s.starttls() 
+          # Authentication 
+          s.login(fromaddr, "ZGFpcw==") 
+          # Converts the Multipart msg into a string 
+          text = msg.as_string() 
+          # sending the mail 
+          s.sendmail(fromaddr, toaddr, text) 
+          # terminating the session 
+          s.quit() 
+          print "foto berhasil"
+          print "done"
+          
+    else:
+      cv2.putText(img, 'tidak dikenali', (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+  cv2.imshow('Face Recognizer',img)
+  k = cv2.waitKey(30) & 0xff
+  if k == 27:
+    break
+
+cap.release()
+cv2.destroyAllWindows()
